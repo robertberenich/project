f3fe23e752ba3f3f0a90b41d6f43f6daddfba61e
Berenich Robert <berenich.robert@gmail.com>
2021-08-28 18:41:14 +0300

    destructor

--- project/Program.cs
+++ project/Program.cs
@@ -11,45 +11,38 @@ namespace project
     {
         static void Main(string[] args)
         {
-            string[] words = {
-                "home",
-                "programming",
-                "victory",
-                "C#",
-                "football",
-                "sport",
-                "book",
-                "learn",
-                "dream",
-                "fun"
-            };
+            string[] finalists = { "James Van", "John Smith", "Leyla Brown", "Tom Homerton", "Bob Douglas" };
 
-            string letter = Console.ReadLine();
+            int winner = Convert.ToInt32(Console.ReadLine());
 
-            int count = 0;
-            foreach (string i in words)
-            {
-                if (i.Contains(letter))
-                {
-                    Console.WriteLine(i);
-                    count++;
-                }
-            }
-            if (count == 0)
-            {
-                Console.WriteLine("No match");
-            }
-            //your code goes here
-
-            /*The program you are given defines an array with 10 words and takes a letter as input.
-            Write a program to iterate through the array and output words containing the taken letter.
-            If there is no such word, the program should output "No match".
-
-            Sample Input
-            u
-
-            Sample Output
-            fun*/
+            
+            FinalRound finalRound = new FinalRound(finalists, winner);
         }
     }
+
+    class FinalRound
+    {
+        public FinalRound(string[] finalists, int winner)
+        {  
+            Console.WriteLine("Winner is " + finalists[winner]);
+        }
+
+        ~FinalRound()
+        {
+            Console.WriteLine("Game Over");
+        }
+
+
+    }
+
+    /*Five participants have advanced to the final round of a TV trivia show, and it is time to announce the winner and end the game.
+    The program you are given defines the array of finalists, takes the index of the winner as input, and creates the FinalRound object.
+    Complete the FinalRound class by writing the needed code in constructor to take the array and the winner index as parameters and output the corresponding message to show the winner.Then create a destructor to finish the game and output "Game Over".
+
+    Sample Input
+    2
+
+    Sample Output
+    Winner is Leyla Brown
+    Game Over*/
 }
