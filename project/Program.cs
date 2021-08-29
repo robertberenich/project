c2af6573adcd096ba8c3ddcfa99f0088f4af5fc7
Berenich Robert <berenich.robert@gmail.com>
2021-08-29 16:33:42 +0300

    Method Parameters

--- project/Program.cs
+++ project/Program.cs
@@ -11,26 +11,37 @@ namespace project
     {
         static void Main(string[] args)
         {
-            int length = Convert.ToInt32(Console.ReadLine());
-            int height = Convert.ToInt32(Console.ReadLine());
+            int totalPrice = Convert.ToInt32(Console.ReadLine());
+
+            double answer = Discount(totalPrice);
+
+            Console.WriteLine(answer);
 
 
-            //Output
-            Console.WriteLine(Area(length, height));
         }
-        static int Area(int x, int y)
+        //complete the method declaration
+        static double Discount(int x)
         {
-            return x * y;
+            double result = x;
+            if (x >= 10000)
+            {
+                result = x - (x * 0.2);
+            }
+            return result;
+            //complete the method body
+
         }
     }
 }
-    /*You are writing a program to calculate the area of a rectangle. Currently it takes the the length and the height as inputs.
-    Complete the given method to take them as arguments, then calculate and return the area.
+    /*A store is running a promotion: if the total purchase price is equal to or exceeds
+    10000, the price will be discounted by 20%.
+    The program you are given takes the total purchase price as input.
+    Complete the given method to take the total purchase price as an argument, and calculate and return the discounted price if campaign's requirement is satisfied.
+    The method should return the same price if discount is not given.
 
     Sample Input
-    4
-    5
+    13000
 
     Sample Output
-    20
+    10400
     */
