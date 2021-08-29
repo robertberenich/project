0e57b04bc98d77d721f16fc5410a4ac0cb99d508
Berenich Robert <berenich.robert@gmail.com>
2021-08-29 16:13:55 +0300

    Array Properties & Methods 1

--- project/Program.cs
+++ project/Program.cs
@@ -11,50 +11,26 @@ namespace project
     {
         static void Main(string[] args)
         {
-            string name1 = Console.ReadLine();
-            int points1 = Convert.ToInt32(Console.ReadLine());
-            string name2 = Console.ReadLine();
-            int points2 = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
 
-            DancerPoints dancer1 = new DancerPoints(name1, points1);
-            DancerPoints dancer2 = new DancerPoints(name2, points2);
-
-            DancerPoints total = dancer1 + dancer2;
-            Console.WriteLine(total.name);
-            Console.WriteLine(total.points);
+            int max = array.Max();
+            int min = array.Min();
+            Console.WriteLine(max + min);
         }
-    }
-
-    class DancerPoints
-    {
-        public string name;
-        public int points;
-        public DancerPoints(string name, int points)
-        {
-            this.name = name;
-            this.points = points;
-        }
-
-        public static DancerPoints operator + (DancerPoints dancer1, DancerPoints dancer2)
-        {
-            string nameLocal = $"{dancer1.name} & {dancer2.name}";
-            int pointsLocal = dancer1.points + dancer2.points;
-            DancerPoints duo = new DancerPoints(nameLocal, pointsLocal);
-            return duo;
-        }
-
     }
 }
-    /*In a ballroom dancing competition, each dancer from a pair is evaluated separately, and then their points are summed up to get the total pair score.
-    The program you are given takes the names and the points of each dancer as input and creates a DancerPoints objects for each dancer, using the taken name and score values as parameters for constructors.
-    Complete the given class, using overload + operator to return an new object where the names of dancers are in one string (see sample output) and the score is equal to the sum of their points.
+            /*Write a program to take 5 numbers as input, then calculate and output the sum of the maximum and the minimum inputted values.
 
-    Sample Input
-    Dave
-    8
-    Jessica
-    7
+            Sample Input
+            5
+            6
+            14
+            2
+            1
 
-    Sample Output
-    Dave & Jessica
-    15*/
+            Sample Output
+            15*/
