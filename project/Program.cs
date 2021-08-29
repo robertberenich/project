2e66471ef7b28a555f98ab8193553e7398928d92
Berenich Robert <berenich.robert@gmail.com>
2021-08-29 16:15:14 +0300

    Jagged Arrays

--- project/Program.cs
+++ project/Program.cs
@@ -11,26 +11,44 @@ namespace project
     {
         static void Main(string[] args)
         {
-            int[] array = new int[5];
-            for (int i = 0; i < 5; i++)
-            {
-                array[i] = Convert.ToInt32(Console.ReadLine());
-            }
+            int day1Winner = Convert.ToInt32(Console.ReadLine());
+            int day2Winner = Convert.ToInt32(Console.ReadLine());
+            int day3Winner = Convert.ToInt32(Console.ReadLine());
 
-            int max = array.Max();
-            int min = array.Min();
-            Console.WriteLine(max + min);
+            int[] sum = new int[3];
+            sum[0] = day1Winner;
+            sum[1] = day2Winner;
+            sum[2] = day3Winner;
+
+            string[][] olympiad = new string[][]
+            {
+                //day 1 - 5 participants
+                new string[] { "Jill Yan", "Bridgette Ramona", "Sree Sanda", "Jareth Charlene", "Carl Soner" },
+                //day 2 - 7 participants
+                new string[] { "Anna Hel", "Mariette Vedrana", "Fran Mayur", "Drake Hilmar", "Nikolay Brooks", "Eliana Vlatko", "Villem Mario" },
+                //day 3 - 4 participants
+                new string[] { "Hieremias Zavia", "Ziya Ollie", "Christoffel Casper", "Kristian Dana", }
+
+            };
+            //your code goes here
+            for (int i = 0; i < 3; i++)
+            {
+                int winner = sum[i] - 1;
+                Console.WriteLine(olympiad[i][winner]);
+            }
         }
     }
 }
-            /*Write a program to take 5 numbers as input, then calculate and output the sum of the maximum and the minimum inputted values.
+        /*The qualifiers for the Olympiad lasts 3 days, and one winner is selected each qualifying day.
+        The jagged array you are given represents the list of all participants, divided by the number of days
+        Write a program to take the numbers of each day's winners as input and output them.
 
-            Sample Input
-            5
-            6
-            14
-            2
-            1
+        Sample Input
+        2
+        3
+        4
 
-            Sample Output
-            15*/
+        Sample Output
+        Bridgette Ramona
+        Fran Mayur
+        Kristian Dana*/
