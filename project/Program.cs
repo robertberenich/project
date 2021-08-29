8cbdd5757cbef24836a40350a8672e345a57199c
Berenich Robert <berenich.robert@gmail.com>
2021-08-29 18:32:07 +0300

    Protected Members

--- project/Program.cs
+++ project/Program.cs
@@ -11,45 +11,50 @@ namespace project
     {
         static void Main(string[] args)
         {
-            string brandName = Console.ReadLine();
-            string modelName = Console.ReadLine();
+            string accNumber = Console.ReadLine();
+            double balance = Convert.ToDouble(Console.ReadLine());
 
-            Car car = new Car();
-            car.Brand = brandName;
-            car.Model = modelName;
+            User user = new User(accNumber, balance);
 
-            car.ShowBrand();
-            car.ShowModel();
-        }
-    }
-    class Vehicle
-    {
-        public string Brand { get; set; }
+            user.ShowDetails();
 
-        public void ShowBrand()
-        {
-            Console.WriteLine("Brand: " + Brand);
         }
     }
 
-    //complete the Car class
-    class Car: Vehicle
+    class Account
     {
-        public string Model { get; set; }
-        public void ShowModel ()
+        protected double Balance { get; set; }
+    }
+
+    class User : Account
+    {
+        public string AccNumber { get; set; }
+
+        //complete the constructor
+        public User(string accNumber, double balance)
         {
-            Console.WriteLine("Model: " + Model);
+            this.AccNumber = accNumber;
+            this.Balance = balance;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("Account N: " + AccNumber);
+            Console.WriteLine("Balance: " + Balance);
         }
     }
 }
-    /*The program you are given takes the brand and model of the car as input, and defines a Vehicle class with model property and ShowModel() method.
-    Complete the Car class to inherit the Vehicle class, and add the Model property and ShowModel() method so that the given method call of the car object works correctly(see sample output).
+    /*
+    The program you are given takes an account number and its balance as input.
+    It defines Account class with 1 member balance and derives User class from it with 1 additional member - the account number, then creates a user object and tries to store the balance and account number in it, and shows the details. But something is wrong.
+    Fix the program so that it completes the User() constructor, which should assign the parameters to the corresponding members of the User class.
+    Also, check the access modifier of balance member of Account class.
 
     Sample Input
-    BMW
-    5 Series
+    005615216
+    1488.36
 
     Sample Output
-    Brand: BMW
-    Model: 5 Series
+    Account N: 005615216
+    Balance: 1488.36
     */
