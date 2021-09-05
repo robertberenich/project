17b35ed7b59af303a191adcb9be26cb510df983d
Berenich Robert <berenich.robert@gmail.com>
2021-09-05 19:52:20 +0300

    try catch

--- project/Program.cs
+++ project/Program.cs
@@ -11,54 +11,33 @@ namespace project
     {
         static void Main(string[] args)
         {
-
-            Player player1 = new Player(Difficulty.Easy);
-            Player player2 = new Player(Difficulty.Medium);
-            Player player3 = new Player(Difficulty.Hard);
-        }
-    }
-
-    /*
-    Easy => "You have 3 minutes 45 seconds"
-    Medium = > "You have 3 minutes 20 seconds"
-    Hard => "You have 3 minutes"
-    */
-
-    class Player
-    {
-        public Player(Difficulty x)
-        {
-            switch (x)
+            string[] tours = { "England", "Spain", "Italy", "Portugal", "France" };
+            int choice = Convert.ToInt32(Console.ReadLine());
+            try
             {
-                case Difficulty.Easy:
-                    Console.WriteLine("You have 3 minutes 45 seconds");
-                    break;
-                case Difficulty.Medium:
-                    Console.WriteLine("You have 3 minutes 20 seconds");
-                    break;
-                case Difficulty.Hard:
-                    Console.WriteLine("You have 3 minutes");
-                    break;
-                default:
-                    break;
+                Console.WriteLine($"{tours[choice]}");
+            }
+            catch
+            {
+                Console.WriteLine("Wrong number");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye");
             }
 
         }
     }
-    enum Difficulty
-    {
-        Easy,
-        Medium,
-        Hard
-    };
 
 }
-/*A racing video game has 3 difficulty levels: Easy, Medium, and Hard.
-Each difficulty level is assigned maximum time to complete the track: the higher the difficulty, the lower the time.
-The program you are given defines Player class and Difficulty enum, and creates 3 Player objects with different difficulties as parameter for the constructor.
-Complete the Player constructor, which takes the enum as a parameter to check the time for each difficulty option and outputs the corresponding message:
-Easy => "You have 3 minutes 45 seconds"
-Medium = > "You have 3 minutes 20 seconds"
-Hard => "You have 3 minutes"
+/*A tour operator offers package holidays in England, Spain, Italy, Portugal and France.
+The program you are given defines an array with those options and takes N number as input.
+Write a program to output the package option with N index. If the number is out of range, program should output "Wrong number". Regardless of the option results, the program should output "Goodbye" at the end.
 
+Sample Input
+2
+
+Sample Output
+Italy
+Goodbye
 */
