0a7abede941fda7f4839725d871f9928a10daa09
Berenich Robert <berenich.robert@gmail.com>
2021-09-05 20:01:53 +0300

    linq_intro(nonLINQvsLINQ variants)

--- project/Program.cs
+++ project/Program.cs
@@ -1,51 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
-namespace project
+namespace linq
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double discount = Convert.ToInt32(Console.ReadLine());
+            string path = @"C:\windows";
 
-            Dictionary<string, int> coffee = new Dictionary<string, int>();
-            coffee.Add("Americano", 50);
-            coffee.Add("Latte", 70);
-            coffee.Add("Flat White", 60);
-            coffee.Add("Espresso", 60);
-            coffee.Add("Cappuccino", 80);
-            coffee.Add("Mocha", 90);
 
-            string[] s = coffee.Keys.ToArray();
-            foreach (string i in s)
+            jobMethod2(path);
+            //jobMethod(path);
+
+
+        }
+
+        private static void jobMethod2(string path)
+        {
+            var query = new DirectoryInfo(path).GetFiles()
+                .OrderByDescending(f => f.Length)
+                .Take(5);
+            foreach (var file in query)
             {
-                double num = coffee[i];
-                double res = num - (num * (discount / 100));
-                coffee[i] = (int)Math.Ceiling(res);
+                Console.WriteLine($"{file.Name}: {file.Length}");
             }
-            foreach (string item in coffee.Keys)
+        }
+
+        private static void jobMethod(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new CompareTool());
+
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine(item + ": " + coffee[item]);
+                FileInfo sample = files[i];
+                Console.WriteLine($"{sample.Name}: {sample.Length}");
             }
         }
     }
+    public class CompareTool : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
+        }
+    }
 }
-/*A coffee shop manager is running a promotion and wants to offer a discount for coffee drinks.
-The program you are given takes the discount value as input and defines a dictionary, where the names of the coffee drinks are set as keys, and their prices are set as values.
-Write a program to discount all of the prices and output a new price list in the format shown below.
-
-Sample Input
-10
-
-Sample Output
-Americano: 45
-Latte: 63
-Flat White: 54
-Espresso: 54
-Cappuccino: 72
-Mocha: 81*/
