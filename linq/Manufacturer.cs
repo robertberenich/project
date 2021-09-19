2074a1381a80cba9f2bb0a6211d801b07d31db29
Berenich Robert <berenich.robert@gmail.com>
2021-09-19 21:19:48 +0300

    linq2

---
+++ linq/Manufacturer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq
+{
+    public class Manufacturer
+    {
+        public string Name { get; set; }
+        public string Headquarters { get; set; }
+        public int Year{ get; set; }
+
+        internal static Manufacturer processInfo(string line)
+        {
+            var separatedLine = line.Split(',');
+
+            return new Manufacturer
+            {
+                Name = separatedLine[0],
+                Headquarters = separatedLine[1],
+                Year = int.Parse(separatedLine[2])
+            };
+        }
+    }
+}
---
+++ linq/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            var manufacturers = ReadAllInfo("manufacturers.csv");
+            var cars = ReadAllfuel("fuel.csv");
+
+            var queryCars =
+                manufacturers.Where(c => c.Headquarters == "Japan")
+                .Select(c => c);
+
+            var queryFuel =
+                cars.Where(c => c.Manufacturer == "Audi")
+                .OrderByDescending(c => c.Combined)
+                .ThenBy(c => c.Name)
+                .Select(c => new
+                {
+                    c.Combined,
+                    c.Name,
+                    c.Manufacturer
+                });
+            var queryJoinExtensionMethod =
+                cars.Join(manufacturers,
+                c=>c.Manufacturer,
+                m=>m.Name,
+                (c,m) => new
+                {
+                    c.Combined,
+                    c.Name,
+                    m.Headquarters
+                })
+                .OrderByDescending(c => c.Combined)
+                .ThenBy(c => c.Name);
+
+            var queryDouleJoinExtensionMethod =
+                cars.Join(manufacturers,
+                c => new { c.Manufacturer, c.Year},
+                m => new { Manufacturer = m.Name, m.Year},
+                (c, m) => new
+                {
+                    c.Combined,
+                    c.Name,
+                    m.Headquarters
+                })
+                .OrderByDescending(c => c.Combined)
+                .ThenBy(c => c.Name);
+
+            //foreach (var item in queryJoinExtensionMethod.Take(10))
+            //{
+            //    Console.WriteLine($"{item.Name}: {item.Headquarters}, {item.Combined}");
+            //}
+
+            //var queryJoinTest =
+            //    from car in cars
+            //    join manufacturer in manufacturers
+            //        on car.Manufacturer equals manufacturer.Name
+            //    orderby car.Combined descending, car.Name
+            //    select new
+            //    {
+            //        manufacturer.Headquarters,
+            //        car.Name,
+            //        car.Manufacturer
+            //    };
+
+            //foreach (var item in queryCars)
+            //{
+            //    Console.WriteLine(item.Headquarters);
+            //}
+
+            //foreach (var item in queryFuel)
+            //{
+            //    Console.WriteLine($"{item.Name}: {item.Combined}, {item.Manufacturer}");
+            //}
+
+            //foreach (var item in queryJoinTest.Take(10))
+            //{
+            //    Console.WriteLine($"{item.Name}: {item.Headquarters}, {item.Manufacturer}");
+            //}
+
+            //==========================================================================================================================================================================================================================================================
+
+            var queryGroup =
+                cars.GroupBy(c => c.Manufacturer.ToUpper())
+                .OrderBy(g => g.Key);
+
+            var queryJoingGroup =
+                manufacturers.GroupJoin(cars,
+                m => m.Name,
+                c => c.Manufacturer, (m, g) =>
+                new
+                {
+                    Manufacturer = m,
+                    Cars = g
+                });
+
+            //foreach (var item in queryGroup)
+            //{
+            //    Console.WriteLine(item.Key);
+            //    foreach (var car in item.OrderByDescending(c => c.Combined).Take(2))
+            //    {
+            //        Console.WriteLine($"\t{car.Name} : {car.Combined}");
+            //    }
+            //}
+
+            
+        }
+
+        private static List<Car> ReadAllfuel(string path)
+        {
+            return
+                File.ReadAllLines(path)
+                .Skip(1)
+                .Where(line => line.Length > 0)
+                .Select(Car.ProcessData)
+                .ToList();
+        }
+
+        private static List<Manufacturer> ReadAllInfo(string path)
+        {
+            return
+                File.ReadAllLines(path)
+                .Skip(1)
+                .Where(line => line.Length > 0)
+                .Select(Manufacturer.processInfo)
+                .ToList();
+        }
+
+        
+    }
+}
