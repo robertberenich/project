39784559a33cdef8f542c6756155d7499cff3b28
Berenich Robert <berenich.robert@gmail.com>
2021-09-21 12:54:59 +0300

    xml

---
+++ xml/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xml
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Displacement { get; set; }
+        public int Cylinders{ get; set; }
+        public int City { get; set; }
+        public int Highway { get; set; }
+        public int Combined{ get; set; }
+
+        internal static Car ProcessData(string line)
+        {
+            var separatedLine = line.Split(',');
+            return new Car
+            {
+                Year = int.Parse(separatedLine[0]),
+                Manufacturer = separatedLine[1],
+                Name = separatedLine[2],
+                Displacement = double.Parse(separatedLine[3]),
+                Cylinders = int.Parse(separatedLine[4]),
+                City = int.Parse(separatedLine[5]),
+                Highway = int.Parse(separatedLine[6]),
+                Combined = int.Parse(separatedLine[7])
+            };
+        }
+    }
+}
---
+++ xml/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace xml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            XlmCreate();
+
+            QueryXml();
+
+        }
+
+        private static void QueryXml()
+        {
+            var ns = (XNamespace)"some namespace";
+            var ex = (XNamespace)"some different namespace";
+            var document = XDocument.Load("fuel.xml");
+
+            var query =
+                document.Element(ns + "Cars").Elements(ex + "Car")
+                .Where(
+                    d => d.Attribute("Manufacturer").Value == "BMW")
+                .Select(d=>d.Attribute("Name").Value);
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void XlmCreate()
+        {
+
+            /*
+             * <Cars>
+             *      <Car>
+             *          <Name>abc</Name>
+             *          <Combined>21</Combined>
+             *          ...
+             *      </Car>
+             *      .... more cars
+             * </Cars>
+             */
+
+            var records = ReadAllFuel("fuel.csv");
+
+            var ns = (XNamespace)"some namespace";
+            var ex = (XNamespace)"some different namespace";
+            var document = new XDocument();
+            var cars = new XElement(ns + "Cars");
+
+            var query =
+                records.Select(r => new XElement(ex + "Car",
+                    new XAttribute("Name", r.Name),
+                    new XAttribute("Combined", r.Combined),
+                    new XAttribute("Manufacturer", r.Manufacturer)
+                    ));
+            cars.Add(query);
+            document.Add(cars);
+            document.Save("fuel.xml");
+
+            //foreach (var record in records)
+            //{
+            //    var car = new XElement("Car");
+            //    var name = new XElement("Name", record.Name);
+            //    var combined = new XElement("Combined", record.Combined);
+
+            //    car.Add(name);
+            //    car.Add(combined);
+            //    cars.Add(car);
+            //}
+
+            //foreach (var record in records)
+            //{
+            //    var car = new XElement("Car",
+            //        new XAttribute("Name", record.Name),
+            //        new XAttribute("Combined", record.Combined),
+            //        new XAttribute("Manufacturer", record.Manufacturer)
+            //        );
+
+            //    cars.Add(car);
+            //}
+
+
+
+            //==================================================================
+            /*
+                 * <Cars>
+                 *      <Car> Name="abc" Combined "21"></Car>
+                 *      .... more cars
+                 * </Cars>
+                 * 
+                 * the same as a previous one but instead of XElement -> XAttribute
+            */
+        }
+
+        private static List<Car> ReadAllFuel(string path)
+        {
+            return
+                File.ReadAllLines(path)
+                .Skip(1)
+                .Where(line => line.Length > 0)
+                .Select(Car.ProcessData)
+                .ToList();
+        }
+    }
+}
